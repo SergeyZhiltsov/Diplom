ce3386b4da047dd7cb441fdbdb14519e4e325810
Siddhartha Gupta <Siddhartha.Gupta@acurian.com>
2017-09-26 13:47:30 -0400

    Added General Health Pages

---
+++ src/test/java/com/acurian/selenium/pages/OLS/generalHealth/AffectYourLungs.java
@@ -0,0 +1,46 @@
+package com.acurian.selenium.pages.OLS.generalHealth;
+
+import java.util.List;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import com.acurian.selenium.pages.OLS.MainPageOLS;
+import ru.yandex.qatools.allure.annotations.Step;
+
+public class AffectYourLungs extends MainPageOLS{
+
+    public final String titleExpected = "Has a healthcare professional ever diagnosed you with any of the following conditions which affect your lungs or breathing?\n" + 
+    		"Please select all that apply.";
+    		
+    @FindBy(xpath = "//div[@class='question']//div[contains(@class,'visible-md-block')]")
+    WebElement titleText;
+
+    @FindBy(xpath = "//span[contains(@class,'visible-md-inline')]/ancestor::label")
+    List<WebElement> checkBoxList;
+
+    public AffectYourLungs() {
+        PageFactory.initElements(getDriver(), this);
+    }
+
+    @Step
+    public AffectYourLungs waitForPageLoad() {
+        waitForPageLoadMain(titleText, titleExpected);
+        return this;
+    }
+
+    @Step
+    public AffectYourLungs clickOnAnswers(String ...answerText) {
+        clickOnCheckBoxes(checkBoxList, answerText);
+//        List<String> answerTextList = Arrays.asList(answerText);
+//        checkBoxList.stream().filter(el -> answerTextList.contains(el.getText()))
+//                .forEach(el -> getActions().moveToElement(el.findElement(By.xpath("ancestor::label")),5,5).click().build().perform());
+//        waitForAnimation();
+        return this;
+    }
+
+    @Step
+    public String getTitleText() {
+        return getText(titleText);
+    }
+
+}
