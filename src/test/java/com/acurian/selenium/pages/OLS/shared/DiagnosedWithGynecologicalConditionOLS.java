4ca0063482ac8ee24a3392b248c4a21465485117
Siddhartha Gupta <Siddhartha.Gupta@acurian.com>
2017-12-04 14:04:27 -0500

    End Module OLS shared Scripts

---
+++ src/test/java/com/acurian/selenium/pages/OLS/shared/DiagnosedWithGynecologicalConditionOLS.java
@@ -0,0 +1,46 @@
+package com.acurian.selenium.pages.OLS.shared;
+
+import java.util.List;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import com.acurian.selenium.pages.OLS.MainPageOLS;
+import ru.yandex.qatools.allure.annotations.Step;
+
+public class DiagnosedWithGynecologicalConditionOLS extends MainPageOLS{
+    
+    public final String titleExpected = "Has a healthcare professional ever diagnosed you with any of these other gynecological or women's health conditions?\n" +
+            "Please select all that apply.";
+    
+    		
+    @FindBy(xpath = "//div[@class='question']//div[contains(@class,'visible-md-block')]")
+    WebElement titleText;
+
+    @FindBy(xpath = "//span[contains(@class,'visible-md-inline')]/span[@class='show-in-ols']")
+    List<WebElement> checkBoxList;
+
+    
+    public DiagnosedWithGynecologicalConditionOLS() {
+        PageFactory.initElements(getDriver(), this);
+    }
+
+    @Step
+    
+    public DiagnosedWithGynecologicalConditionOLS waitForPageLoad() {
+        waitForPageLoadMain(titleText, titleExpected);
+        return this;
+    }
+
+    @Step
+    
+    public DiagnosedWithGynecologicalConditionOLS clickOnAnswers(String answerText) {
+        clickOnCheckBoxes(checkBoxList, answerText);
+        return this;
+    }
+
+    @Step
+    public String getTitleText() {
+        return getText(titleText);
+    }
+
+}
