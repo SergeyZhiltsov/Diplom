4daa2ce83d94332c8f972dd2eda731502d60da77
Siddhartha Gupta <Siddhartha.Gupta@acurian.com>
2017-12-05 16:10:28 -0500

    AS CC module scripts

---
+++ src/test/java/com/acurian/selenium/pages/CC/AS_4319/ResultsOfYouXrayOrMRICC.java
@@ -0,0 +1,45 @@
+package com.acurian.selenium.pages.CC.AS_4319;
+
+import java.util.List;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import com.acurian.selenium.constants.Locators;
+import com.acurian.selenium.pages.CC.MainPageCC;
+
+import ru.yandex.qatools.allure.annotations.Step;
+
+public class ResultsOfYouXrayOrMRICC extends MainPageCC {
+
+    public final String titleExpected = "What did your doctor tell you about the results of your most recent x-ray or MRI?";
+
+    @FindBy(xpath = Locators.BASIC_TITLE_WITH_RADIO_BUTTON_CC)
+    WebElement titleText;
+
+    @FindBy(xpath = Locators.RADIO_BUTTON_LIST_CC)
+    List<WebElement> radioButtonsList;
+
+    public ResultsOfYouXrayOrMRICC() {
+        PageFactory.initElements(getDriver(), this);
+    }
+
+    @Step
+    public ResultsOfYouXrayOrMRICC waitForPageLoad() {
+        waitForPageLoadMain(titleText, titleExpected);
+        return this;
+    }
+
+    @Step
+    public ResultsOfYouXrayOrMRICC clickOnAnswer(String answerText) {
+        clickOnRadioButton(radioButtonsList, answerText);
+        return this;
+    }
+
+    @Step
+    public String getTitleText(){
+        return getText(titleText);
+    }
+
+}
---
+++ src/test/java/com/acurian/selenium/pages/CC/AS_4319/YouBeenDiagnosedWithCC.java
@@ -0,0 +1,43 @@
+package com.acurian.selenium.pages.CC.AS_4319;
+
+import java.util.List;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import com.acurian.selenium.constants.Locators;
+import com.acurian.selenium.pages.CC.MainPageCC;
+import ru.yandex.qatools.allure.annotations.Step;
+
+public class YouBeenDiagnosedWithCC extends MainPageCC{
+
+    public final String titleExpected = "Which of the following have you been diagnosed with?\n" +
+            "Agent Note: Select all that apply";
+
+    @FindBy(xpath = Locators.BASIC_TITLE_WITH_CHECKBOXES_BUTTON_CC)
+    WebElement titleText;
+
+    @FindBy(xpath = Locators.CHEKBOX_LIST_CC)
+    List<WebElement> checkBoxList;
+
+    public YouBeenDiagnosedWithCC() {
+        PageFactory.initElements(getDriver(), this);
+    }
+
+    @Step
+    public YouBeenDiagnosedWithCC waitForPageLoad() {
+        waitForPageLoadMain(titleText, titleExpected);
+        return this;
+    }
+
+    @Step
+    public YouBeenDiagnosedWithCC clickOnAnswers(String ...answerText) {
+        clickOnCheckBoxes(checkBoxList, answerText);
+        return this;
+    }
+
+    @Step
+    public String getTitleText(){
+        return getText(titleText);
+    }
+
+}
