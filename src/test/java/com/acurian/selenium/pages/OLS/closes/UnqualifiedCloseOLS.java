51111a610ffe19bc7c971bdedf615523f06c7b27
Ramya Avunuri <ramya.avunuri@acurian.com>
2018-06-13 12:00:55 -0400

    AMS1_54_changes

---
+++ src/test/java/com/acurian/selenium/pages/OLS/closes/UnqualifiedCloseOLS.java
@@ -0,0 +1,63 @@
+package com.acurian.selenium.pages.OLS.closes;
+
+import com.acurian.selenium.constants.Locators;
+import com.acurian.selenium.constants.Platforms;
+import com.acurian.selenium.pages.OLS.MainPageOLS;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import ru.yandex.qatools.allure.annotations.Step;
+
+import java.util.List;
+
+public class UnqualifiedCloseOLS extends MainPageOLS{
+
+    //Pediatric module
+    public final String titleExpected = "We certainly appreciate your interest in participating. Unfortunately, from the information you have provided, you would not be a candidate for this study.";
+
+    @FindBy(xpath = "//div[@class='question']//div[contains(@class,'visible-md-block')]")
+    WebElement titleText1;
+
+    @FindBy(xpath = "//div[@class='question']//div[contains(@class,'visible-sm-block')]")
+    WebElement titleText2;
+
+    @FindBy(xpath = "//div[@class='question']//div[contains(@class,'visible-xs-block')]")
+    WebElement titleText3;
+
+    WebElement titleText;
+
+    @FindBy(xpath = "//span[contains(@class,'visible-md-inline')]/ancestor::label")
+    List<WebElement> radioButtonsList;
+
+    public UnqualifiedCloseOLS() {
+        PageFactory.initElements(getDriver(), this);
+        switch (Locators.isEnvWeb) {
+            case Platforms.WEB:
+                titleText = titleText1;
+                break;
+            case Platforms.TABLET:
+                titleText = titleText2;
+                break;
+            case Platforms.MOBILE:
+                titleText = titleText3;
+                break;
+        }
+    }
+
+    @Step
+    public UnqualifiedCloseOLS waitForPageLoad() {
+        waitForPageLoadMain(titleText, titleExpected);
+        return this;
+    }
+
+    @Step
+    public UnqualifiedCloseOLS clickOnAnswer(String answerText) {
+        clickOnRadioButton(radioButtonsList, answerText);
+        return this;
+    }
+
+    @Step
+    public String getTitleText(){
+        return getText(titleText);
+    }
+}
