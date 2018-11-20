2bd69d6d3764d5ad63d3065bbdacfb792c7e4be7
Vladyslav Kholod <vladyslav.kholod@qatestlab.net>
2018-11-20 15:08:34 +0200

    add page method for DS asserting

--- src/test/java/com/acurian/selenium/pages/CC/closes/SynexusRadiantDirectScheduleCC.java
+++ src/test/java/com/acurian/selenium/pages/CC/closes/SynexusRadiantDirectScheduleCC.java
@@ -8,6 +8,7 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedCondition;
 
+import org.testng.Assert;
 import ru.yandex.qatools.allure.annotations.Step;
 
 import java.util.List;
@@ -28,6 +29,45 @@ public class SynexusRadiantDirectScheduleCC extends MainPageCC{
 
     @FindBy(xpath = Locators.RADIO_BUTTON_LIST_CC)
     List<WebElement> radioButtonsList;
+
+    @FindBy(xpath = "//div[@class='question_text']/table/tbody/tr[1]/td[1]")
+    WebElement firstNameField;
+
+    @FindBy(xpath = "//div[@class='question_text']/table/tbody/tr[1]/td[2]")
+    WebElement secondNameField;
+
+    @FindBy(xpath = "//div[@class='question_text']/table/tbody/tr[2]/td[1]")
+    WebElement dateOfBirthField;
+
+    @FindBy(xpath = "//div[@class='question_text']/table/tbody/tr[3]/td[1]")
+    WebElement countryField;
+
+    @FindBy(xpath = "//div[@class='question_text']/table/tbody/tr[6]/td[1]")
+    WebElement cityAndStateField;
+
+    @FindBy(xpath = "//div[@class='question_text']/table/tbody/tr[7]/td[1]")
+    WebElement emailField;
+
+    @FindBy(xpath = "//div[@class='question_text']/table/tbody/tr[8]/td[1]")
+    WebElement phoneNumberField;
+
+    @FindBy(xpath = "//div[@class='question_text']/table/tbody/tr[6]/td[2]")
+    WebElement zipCodeField;
+
+    @FindBy(xpath = "//div[@class='question_text']")
+    WebElement allText;
+
+
+    //cannot determine because they are nodes
+//    @FindBy(xpath = "")
+//    WebElement siteCode;
+//
+//    @FindBy(xpath = "")
+//    WebElement siteName;
+//
+//    @FindBy(xpath = "")
+//    WebElement studyName;
+
 
     public SynexusRadiantDirectScheduleCC() {
         PageFactory.initElements(getDriver(), this);
@@ -51,7 +91,24 @@ public class SynexusRadiantDirectScheduleCC extends MainPageCC{
         clickOnRadioButton(radioButtonsList, answerText);
         return this;
     }
-    
+
+    @Step
+    public SynexusRadiantDirectScheduleCC assertVariables(String firstName, String secondName, String dateOfBirth, String country,
+                                                          String cityAndState, String zipCode, String email, String phoneNumber,
+                                                          String siteNumber, String siteName, String studyName) {
+        Assert.assertEquals(firstNameField.getText(), "Patient First Name:" + firstName);
+        Assert.assertEquals(secondNameField.getText(), "Patient First Name:" + secondName);
+        Assert.assertEquals(dateOfBirthField.getText(), "Patient First Name:" + firstName);
+        Assert.assertEquals(countryField.getText(), "Patient First Name:" + firstName);
+        Assert.assertEquals(cityAndStateField.getText(), "Patient First Name:" + firstName);
+        Assert.assertEquals(emailField.getText(), "Patient First Name:" + firstName);
+        Assert.assertEquals(phoneNumberField.getText(), "Patient First Name:" + firstName);
+        Assert.assertEquals(zipCodeField.getText(), "Patient First Name:" + firstName);
+        Assert.assertTrue(allText.getText().contains("Site: " + siteNumber + " - " + siteName));
+        Assert.assertTrue(allText.getText().contains("Study: " + studyName));
+        return this;
+    }
+
 
     @Step
     public String getTitleText(){
