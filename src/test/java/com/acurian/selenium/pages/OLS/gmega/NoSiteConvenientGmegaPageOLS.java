9e0eac2ea647e2d748b8064beb3d522909e93ca3
Vladyslav Kholod <vladyslav.kholod@qatestlab.net>
2018-12-07 18:54:16 +0200

    completed dispo

---
+++ src/test/java/com/acurian/selenium/pages/OLS/gmega/NoSiteConvenientGmegaPageOLS.java
@@ -0,0 +1,42 @@
+package com.acurian.selenium.pages.OLS.gmega;
+
+import com.acurian.selenium.constants.Locators;
+import com.acurian.selenium.pages.OLS.MainPageOLS;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import ru.yandex.qatools.allure.annotations.Step;
+
+import java.util.List;
+
+public class NoSiteConvenientGmegaPageOLS extends MainPageOLS {
+
+    public final String titleExpected = "We are sorry that no study site is convenient for you. We will contact you if a more convenient site becomes available.";
+
+    @FindBy(xpath = Locators.BASIC_TITLE_WITH_RADIO_BUTTON_OLS)
+    WebElement titleText;
+
+    @FindBy(xpath = Locators.RADIO_BUTTON_LIST_OLS)
+    List<WebElement> radioButtonsList;
+
+    public NoSiteConvenientGmegaPageOLS() {
+        PageFactory.initElements(getDriver(), this);
+    }
+
+    @Step
+    public NoSiteConvenientGmegaPageOLS waitForPageLoad() {
+        waitForPageLoadMain(titleText, titleExpected);
+        return this;
+    }
+
+    @Step
+    public NoSiteConvenientGmegaPageOLS clickOnAnswer(String answerText) {
+        clickOnRadioButton(radioButtonsList, answerText);
+        return this;
+    }
+
+    @Step
+    public String getTitleText(){
+        return getText(titleText);
+    }
+}
---
+++ src/test/java/com/acurian/selenium/tests/dispo/Dispo3CdisqualifyType1.java
@@ -0,0 +1,77 @@
+package com.acurian.selenium.tests.dispo;
+
+import com.acurian.selenium.pages.BaseTest;
+import com.acurian.selenium.pages.OLS.closes.AboutHealthPageOLS;
+import com.acurian.selenium.pages.OLS.closes.UnqualifiedCloseOLS_GMEGA;
+import com.acurian.selenium.pages.OLS.generalHealth.*;
+import com.acurian.selenium.pages.OLS.gmega.ThankYouCloseGmegaOLS;
+import com.acurian.selenium.pages.OLS.shared.DateOfBirthPageOLS;
+import com.acurian.selenium.pages.OLS.shared.GenderPageOLS;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import ru.yandex.qatools.allure.annotations.Description;
+
+public class Dispo3CdisqualifyType1 extends BaseTest {
+
+    @Test(enabled= true)
+    @Description("Dispo_3C_DisQualify_Type1")
+    public void dispo3Ctype1() {
+        String phoneNumber = "AUTGMEGA01";
+        String studyName = "Arthritis, a low back pain study, a rheumatoid arthritis (RA) study, an osteoarthritis";
+//        String siteName = "QSC9005_None";
+        String zipCode = "08204";
+
+        String env = System.getProperty("acurian.env", "STG");
+
+        DateOfBirthPageOLS dateOfBirthPageOLS = new DateOfBirthPageOLS();
+        dateOfBirthPageOLS
+                .openPage(env, phoneNumber)
+                .waitForPageLoad();
+        Assert.assertEquals(dateOfBirthPageOLS.getTitleText(), dateOfBirthPageOLS.titleRA2821Expected, "Title is diff");
+        IdentificationPageOLS identificationPageOLS = dateOfBirthPageOLS
+                .setDate("09091980")
+                .clickNextButton(new IdentificationPageOLS());
+
+        GenderPageOLS genderPageOLS = identificationPageOLS
+                .waitForPageLoadNotQ()
+                .setAllFields("Acurian", "Trial", "qa.acurian@gmail.com", "9999999999", zipCode)
+                .clickNextButton(new GenderPageOLS());
+
+        ApproximateHeightPageOLS approximateHeightPageOLS = genderPageOLS
+                .waitForPageLoadGmega()
+                .clickOnAnswer("Female")
+                .clickNextButton(new ApproximateHeightPageOLS());
+
+        FollowingNeurologicalConditionsPageOLS followingNeurologicalConditionsPageOLS = approximateHeightPageOLS
+                .waitForPageLoad()
+                .setAll("5", "5", "160")
+                .clickNextButton(new FollowingNeurologicalConditionsPageOLS());
+
+        DigestiveConditionsPageOLS digestiveConditionsPageOLS = followingNeurologicalConditionsPageOLS
+                .waitForPageLoad()
+                .clickOnAnswers("None of the above")
+                .clickNextButton(new DigestiveConditionsPageOLS());
+
+        BoneOrJointConditionsPageOLS boneOrJointConditionsPageOLS = digestiveConditionsPageOLS
+                .waitForPageLoad()
+                .clickOnAnswers("None of the above")
+                .clickNextButton(new BoneOrJointConditionsPageOLS());
+
+        UnqualifiedCloseOLS_GMEGA unqualifiedCloseOLS_gmega = boneOrJointConditionsPageOLS
+                .waitForPageLoad()
+                .clickOnAnswers("None of the above")
+                .clickNextButton(new UnqualifiedCloseOLS_GMEGA());
+
+        unqualifiedCloseOLS_gmega
+                .waitForPageLoad()
+                .getPage(new SiteSelectionPageOLS())
+                .getPID()
+                .getPage(unqualifiedCloseOLS_gmega)
+                .clickOnAnswer("No")
+                .clickNextButton(new ThankYouCloseGmegaOLS())
+                .waitForPageLoad()
+                .clickNextButton(new AboutHealthPageOLS())
+                .waitForPageLoad()
+                .pidFromDbToLog(env);
+    }
+}
