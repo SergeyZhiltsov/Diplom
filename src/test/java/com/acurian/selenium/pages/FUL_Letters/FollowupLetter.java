bf4f90b0784e84a8e9c029594c468df65afaed4d
eduardrudko <eduard.rudko@qatestlab.net>
2019-01-10 14:07:03 +0200

    configure InstantFUL for QA

--- src/test/java/com/acurian/selenium/pages/FUL_Letters/FollowupLetter.java
+++ src/test/java/com/acurian/selenium/pages/FUL_Letters/FollowupLetter.java
@@ -22,12 +22,12 @@ public class FollowupLetter extends BasePage {
     private final String[] monthNames = {"January", "February", "March", "April", "May", "June", "July",
             "August", "September", "October", "November", "December"};
 
-    private final String emailContentExpectedENG = monthNames[date.get(Calendar.MONTH)] + " " + date.get(Calendar.DATE) + ", " + date.get(Calendar.YEAR) + "\n" +
+    private final String emailContentExpectedMR = monthNames[date.get(Calendar.MONTH)] + " " + date.get(Calendar.DATE) + ", " + date.get(Calendar.YEAR) + "\n" +
             "Acurian Trial\n" +
             "\n" +
             "Dover, DE 19901\n" +
             "Dear Acurian,\n" +
-            "Thank you for your recent interest in participating in one of our Low Back Pain, Diabetes, Arthritis, Rheumatoid Arthritis clinical research studies.\n" +
+            "Thank you for your recent interest in participating in one of our Arthritis, Low Back Pain, Rheumatoid Arthritis, Diabetes clinical research studies.\n" +
             "We have forwarded your information to the study doctor’s office that you selected. If the study doctor’s office has not already contacted you, they should " +
             "be contacting you within the next few days to further discuss the study and to set up an in-person evaluation.\n" +
             "If you are not contacted within the next 5 business days, please contact them directly.\n" +
@@ -37,10 +37,28 @@ public class FollowupLetter extends BasePage {
             "1 Walnutgrove Rd\n" +
             "Dover, DE 19901\n" +
             "(123) 456-7899\n" +
-            "To allow us to send your medical records to the study doctor, please provide information on the doctors who are treating, or have treated, your Low Back Pain, " +
-            "Diabetes, Arthritis, Rheumatoid Arthritis. Please complete all details required by clicking on the link below. Please click here to learn more.\n" +
+            "To allow us to send your medical records to the study doctor, please provide information on the doctors who are treating, or have treated, your Arthritis, Low Back Pain, " +
+            "Rheumatoid Arthritis, Diabetes. Please complete all details required by clicking on the link below. Please click here to learn more.\n" +
             "Please click here to learn more.\n" +
             "Please be assured that your records will be kept confidential and only shared with the study research facility.\n" +
+            "Clinical research studies greatly contribute to the overall progress in understanding and finding future treatments for diseases and we appreciate your interest in participation.\n" +
+            "The AcurianHealth Team";
+
+    private final String emailContentExpected = monthNames[date.get(Calendar.MONTH)] + " " + date.get(Calendar.DATE) + ", " + date.get(Calendar.YEAR) + "\n" +
+            "Acurian Trial\n" +
+            "\n" +
+            "Brookings, SD 57007\n" +
+            "Dear Acurian,\n" +
+            "Thank you for your recent interest in participating in one of our Arthritis, Low Back Pain, Rheumatoid Arthritis, Diabetes clinical research studies.\n" +
+            "We have forwarded your information to the study doctor’s office that you selected. If the study doctor’s office has not already contacted you, they should be " +
+            "contacting you within the next few days to further discuss the study and to set up an in-person evaluation.\n" +
+            "If you are not contacted within the next 5 business days, please contact them directly.\n" +
+            "The study doctor’s office that you selected is:\n" +
+            "Dr. OldFirst OldLast, MD\n" +
+            "AUT_GEMPERM1\n" +
+            "Loop\n" +
+            "Brookings, SD 57007\n" +
+            "(215) 323-9000\n" +
             "Clinical research studies greatly contribute to the overall progress in understanding and finding future treatments for diseases and we appreciate your interest in participation.\n" +
             "The AcurianHealth Team";
 
@@ -74,7 +92,7 @@ public class FollowupLetter extends BasePage {
     }
 
     @Step
-    public FollowupLetter assertgmailFUL(String pid) {
+    public FollowupLetter assertgmailFUL(String pid, boolean isMedicalRecords) {
         By emailLocator = new By.ByXPath("//div[2]/span/span[contains(text(),'" + pid +"')]");
         WebElement emailTitle = null;
         driver.navigate().to(gmailServiceURL);
@@ -93,7 +111,8 @@ public class FollowupLetter extends BasePage {
             Assert.fail("Email wasn't received");
         }
         System.out.println("Recieved email: " + emailTitle.getText());
-        Assert.assertEquals(emailContent.getText(), emailContentExpectedENG, "Email content is diff");
+        if(isMedicalRecords) Assert.assertEquals(emailContent.getText(), emailContentExpectedMR, "Email content is diff");
+        else Assert.assertEquals(emailContent.getText(), emailContentExpected, "Email content is diff");
         return this;
     }
 }
