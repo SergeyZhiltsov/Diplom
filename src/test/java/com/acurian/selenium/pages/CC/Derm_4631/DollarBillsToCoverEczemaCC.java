5e2ef06cf2f57b8edbcf41e90af9bb5faf4cd08b
eduardrudko <eduard.rudko@qatestlab.net>
2019-01-14 20:22:39 +0200

    add pages for DERM 4815 CC

---
+++ src/test/java/com/acurian/selenium/pages/CC/Derm_4631/DollarBillsToCoverEczemaCC.java
@@ -0,0 +1,40 @@
+package com.acurian.selenium.pages.CC.Derm_4631;
+
+import com.acurian.selenium.pages.CC.MainPageCC;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import ru.yandex.qatools.allure.annotations.Step;
+
+public class DollarBillsToCoverEczemaCC extends MainPageCC {
+    public final String titleExpected = "How many dollar bills would it take to cover all the eczema on your skin?";
+
+    @FindBy(xpath = "//div[@class='question_text']")
+    WebElement titleText;
+
+    @FindBy(xpath = "//div[@class='ddlist_container']//select")
+    WebElement dropDownList;
+
+    public DollarBillsToCoverEczemaCC() {
+        PageFactory.initElements(getDriver(), this);
+    }
+
+    @Step
+    public DollarBillsToCoverEczemaCC waitForPageLoad() {
+        waitForAnimation();
+        driverWait.getWaitDriver().until((ExpectedCondition<Boolean>) w-> titleText.getText().contains(titleExpected));
+        return this;
+    }
+
+    @Step
+    public DollarBillsToCoverEczemaCC selectFromDropDown(String answerText) {
+        selectDropDownListOptionByText(dropDownList, answerText);
+        return this;
+    }
+
+    @Step
+    public String getTitleText(){
+        return getText(titleText);
+    }
+}
