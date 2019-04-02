912aae132cb95478d5cc72d67d7c944c75372a54
Ramya Avunuri <ramya.avunuri@acurian.com>
2019-04-02 16:58:20 -0400

    fix dispo site

--- src/test/java/com/acurian/selenium/tests/dispo/Dispo41C.java
+++ src/test/java/com/acurian/selenium/tests/dispo/Dispo41C.java
@@ -23,7 +23,7 @@ public class Dispo41C extends BaseTest {
         String env = System.getProperty("acurian.env", "STG");
         String studyName = env.equals("QA") ?
                 "Arthritis,a low back pain study,a rheumatoid arthritis (RA)" : "Arthritis, a low back pain study, a rheumatoid arthritis (RA)";
-        String siteName = "AUT_GMEGA_Site";
+        String siteName = "AUT_GMEGA_New"; //"AUT_GMEGA_Site";
         String zipCode = "08204";
 
         DateOfBirthPageOLS dateOfBirthPageOLS = new DateOfBirthPageOLS();
