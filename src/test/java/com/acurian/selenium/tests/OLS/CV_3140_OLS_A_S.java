adc9b13c8d3683a387bf1cbfab8d0456191d5745
Vladyslav Kholod <vladyslav.kholod@qatestlab.net>
2019-04-25 16:59:28 +0300

    fixes

--- src/test/java/com/acurian/selenium/tests/OLS/CV_3140_OLS_A_S.java
+++ src/test/java/com/acurian/selenium/tests/OLS/CV_3140_OLS_A_S.java
@@ -192,7 +192,7 @@ public class CV_3140_OLS_A_S extends BaseTest {
         subquestionExperiencedHeartPageOLS
                 .waitForPageLoad(1, subquestionExperiencedHeartPageOLS.titleExpected1)
                 .waitForPageLoad(2, subquestionExperiencedHeartPageOLS.titleExpected2)
-                .waitForPageLoad(3, subquestionExperiencedHeartPageOLS.titleExpected5)
+                .waitForPageLoad(3, subquestionExperiencedHeartPageOLS.titleExpected4)
                 .clickOnAnswerForSubQuestion("When was the last time that you experienced a heart attack?", "Less than 30 days ago")
                 .clickOnAnswerForSubQuestion("When was the last time that you experienced a stroke?", "More than 6 months ago")
                 .clickOnAnswerForSubQuestion("When was the last time that you experienced a TIA or mini-stroke?", "More than 6 months ago")
