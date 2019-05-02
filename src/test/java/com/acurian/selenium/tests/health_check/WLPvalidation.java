d52380011d179d398626a8d654c0be72486039da
Roman Gorbenko <rgorbenko@acurian.com>
2019-05-02 13:53:05 +0300

    WLP updated.

--- src/test/java/com/acurian/selenium/tests/health_check/WLPvalidation.java
+++ src/test/java/com/acurian/selenium/tests/health_check/WLPvalidation.java
@@ -59,6 +59,6 @@ public class WLPvalidation extends BaseTest {
                 .switchTab();
         dateOfBirthPageOLS
                 .waitForPageLoad2Ver();
-        Assert.assertEquals(dateOfBirthPageOLS.getTitleTextVer3(), dateOfBirthPageOLS.titleGHExpected, "Title is diff");
+        Assert.assertEquals(dateOfBirthPageOLS.getTitleTextVer3(), dateOfBirthPageOLS.getExpectedModifiedTitle("a study", "1,000", true), "Title is diff");
     }
 }
