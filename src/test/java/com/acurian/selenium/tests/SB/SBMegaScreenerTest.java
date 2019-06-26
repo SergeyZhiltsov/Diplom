41ad03c397016bca4ddfe6763c247593ade26cbf
Roman Gorbenko <rgorbenko@acurian.com>
2019-06-26 21:39:50 +0300

    Snapshot script for Megascreener.

--- src/test/java/com/acurian/selenium/tests/SB/SBMegaScreenerTest.java
+++ src/test/java/com/acurian/selenium/tests/SB/SBMegaScreenerTest.java
@@ -13,11 +13,17 @@ import ru.yandex.qatools.allure.annotations.Description;
 
 
 public class SBMegaScreenerTest extends BaseTest {
+    String env = System.getProperty("acurian.env", "QA");
     final String studyName = "AUTSBMG - Research Study (Copy)";
     final String projectCode = "AUTSBMG";
-    final String studyId = "8103";
+    String studyId = null;
+    String alertMessage = null;
     final String therapeuticName = "Gastrointestinal";
     final String indicationName = "Arthritis";
+
+    private SBMegaScreenerTest() {
+        setEnvData(env);
+    }
 
     @BeforeMethod
     public void setUp() {
@@ -29,30 +35,39 @@ public class SBMegaScreenerTest extends BaseTest {
         super.tearDown();
     }
 
-    @Test(dataProvider = "SBUserCredentials", dataProviderClass = DataProviderPool.class)
+    @Test(enabled = false, priority = -1, dataProvider = "SBUserCredentials", dataProviderClass = DataProviderPool.class)
     @Description("Basic MS project changes test (deletion data)")
     public void initialProjectChanges(String username, String password) {
         final String env = System.getProperty("acurian.env", "QA");
-        final String questionText = "Let's check (some changes in text) if you are eligible for a clinical research study!";
+        final String questionText = "Are you providing information for yourself or on behalf of someone else? (some changes in text)";
 
         LoginSBPage loginSBPage = new LoginSBPage();
+        StudyEditPage studyEditPage = new StudyEditPage();
 
         loginSBPage.openPage(env).loginAs(username, password)
                 .searchForStudy(studyName)
                 .clickOnStudyName(studyName)
-                .deleteIndication(indicationName)
-                .deleteTherapeutic(therapeuticName)
-                .clickSave()
-                .checkSaveAlertMessage(String.format("%s saved Successfully", projectCode))
+//                .deleteIndication(indicationName)
+//                .deleteTherapeutic(therapeuticName)
+//                .clickSave()
+//                .checkSaveAlertMessage(String.format("%s saved Successfully", projectCode))
         //Question changes
                 .clickQuestionBuilderLink()
-                .clickIntroLink("1")
-                .typeQuestionTextInVisibleField(questionText, 2)
+                .clickIntroLink("5")
+                .typeQuestionTextInVisibleField(questionText, 1)
                 .clickSaveQuestion()
                 .clickSave()
                 .checkStudyAlertMessage(String.format("×\nQuestions for study %s saved Successfully", projectCode));
+        //Logic changes
+        studyEditPage
+                .clickLogicBuilderLink()
+                .clickCoreLink()
+                .clickSubCoreLink(2)
+                .clickFlowLogicOption(1)
+                .selectActionForCoreAndRule(1, 2, "Core-QS3")
+                .clickSaveLogic()
+                .checkAlertMessage("Saved all modified logic Successfully !!!");
         //Public study
-        StudyEditPage studyEditPage = new StudyEditPage();
         studyEditPage
                 .clickDashboard()
                 .clickPublishStudySetup(studyName, StudyProjectsListPage.SetupEnv.valueOf(env))
@@ -62,11 +77,10 @@ public class SBMegaScreenerTest extends BaseTest {
                 .clickConfirmPublishOnPopUp()
                 .clickPublishToEnvironment()
                 .checkAlertMessage(String.format("×\n%s published to %s Successfully. Cleared Cache for Study %s successfully.",
-                        projectCode, (env.equals("QA") ? env : "PROD"), studyId));
+                        projectCode, alertMessage, studyId));
     }
 
-    @Test(dataProvider = "SBUserCredentials", dataProviderClass = DataProviderPool.class,
-             dependsOnMethods = "initialProjectChanges")
+    @Test(enabled = false, priority = 1, dataProvider = "SBUserCredentials", dataProviderClass = DataProviderPool.class)
     @Description("Basic MS project changes test (addition data)")
     public void revertProjectChanges(String username, String password) {
         final String env = System.getProperty("acurian.env", "QA");
@@ -99,14 +113,30 @@ public class SBMegaScreenerTest extends BaseTest {
                 .clickConfirmPublishOnPopUp()
                 .clickPublishToEnvironment()
                 .checkAlertMessage(String.format("×\n%s published to %s Successfully. Cleared Cache for Study %s successfully.",
-                        projectCode, (env.equals("QA") ? env : "PROD"), studyId));
+                        projectCode, alertMessage, studyId));
     }
 
-    @Test()
+    @Test(enabled = false, priority = 0, dependsOnMethods = "initialProjectChanges")
     @Description("Run_MegaScreener_AUTSBMG")
-    public void run_AUTSBMG()
-    {
+    public void run_AUTSBMG() {
         SB_AUTSBMG sb_MG = new SB_AUTSBMG();
         sb_MG.sb_AUTSBMG();
+    }
+
+    private void setEnvData(String env) {
+        switch (env) {
+            case "PRD":
+                studyId = "";
+                alertMessage = "PROD";
+                break;
+            case "STG":
+                studyId = "";
+                alertMessage = "STAGING";
+                break;
+            case "QA":
+            default:
+                studyId = "8103";
+                alertMessage = "QA";
+        }
     }
 }
