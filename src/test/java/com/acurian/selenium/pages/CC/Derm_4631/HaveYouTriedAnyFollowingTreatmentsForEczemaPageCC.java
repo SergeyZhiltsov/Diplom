93884e54dd0fbf20b0413f883e59912a701b6c46
Roman Gorbenko <rgorbenko@acurian.com>
2019-07-02 19:37:45 +0300

    The 4967 OLS and CC were created.

---
+++ src/test/java/com/acurian/selenium/pages/CC/Derm_4631/HaveYouTriedAnyFollowingTreatmentsForEczemaPageCC.java
@@ -0,0 +1,42 @@
+package com.acurian.selenium.pages.CC.Derm_4631;
+
+import com.acurian.selenium.constants.Locators;
+import com.acurian.selenium.pages.CC.MainPageCC;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import ru.yandex.qatools.allure.annotations.Step;
+
+import java.util.List;
+
+public class HaveYouTriedAnyFollowingTreatmentsForEczemaPageCC extends MainPageCC {
+    public final String titleExpected = "Have you tried any of the following treatments for your eczema (atopic dermatitis)?\n" +
+            "Agent Note: Select all that apply";
+
+    @FindBy(xpath = Locators.BASIC_TITLE_WITH_CHECKBOXES_BUTTON_CC)
+    WebElement titleText;
+
+    @FindBy(xpath = Locators.CHEKBOX_LIST_CC)
+    List<WebElement> checkBoxList;
+
+    public HaveYouTriedAnyFollowingTreatmentsForEczemaPageCC() {
+        PageFactory.initElements(getDriver(), this);
+    }
+
+    @Step
+    public HaveYouTriedAnyFollowingTreatmentsForEczemaPageCC waitForPageLoad() {
+        waitForPageLoadMain(titleText, titleExpected);
+        return this;
+    }
+
+    @Step
+    public HaveYouTriedAnyFollowingTreatmentsForEczemaPageCC clickOnAnswers(String ...answerText) {
+        clickOnCheckBoxes(checkBoxList, answerText);
+        return this;
+    }
+
+    @Step
+    public String getTitleText(){
+        return getText(titleText);
+    }
+}
---
+++ src/test/java/com/acurian/selenium/pages/OLS/Derm/HaveYouTriedAnyFollowingTreatmentsForEczemaPageOLS.java
@@ -0,0 +1,43 @@
+package com.acurian.selenium.pages.OLS.Derm;
+
+import com.acurian.selenium.constants.Locators;
+import com.acurian.selenium.pages.OLS.MainPageOLS;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import ru.yandex.qatools.allure.annotations.Step;
+
+import java.util.List;
+
+public class HaveYouTriedAnyFollowingTreatmentsForEczemaPageOLS extends MainPageOLS {
+
+    public final String titleExpected = "Have you tried any of the following treatments for your eczema (atopic dermatitis)?\n" +
+            "Please select all that apply.";
+
+    @FindBy(xpath = Locators.BASIC_TITLE_WITH_CHECKBOXES_BUTTON_OLS)
+    WebElement titleText;
+
+    @FindBy(xpath = Locators.CHEKBOX_LIST_OLS)
+    List<WebElement> checkBoxList;
+
+    public HaveYouTriedAnyFollowingTreatmentsForEczemaPageOLS() {
+        PageFactory.initElements(getDriver(), this);
+    }
+
+    @Step
+    public HaveYouTriedAnyFollowingTreatmentsForEczemaPageOLS waitForPageLoad() {
+        waitForPageLoadMain(titleText, titleExpected);
+        return this;
+    }
+
+    @Step
+    public HaveYouTriedAnyFollowingTreatmentsForEczemaPageOLS clickOnAnswers(String ...answerText) {
+        clickOnCheckBoxes(checkBoxList, answerText);
+        return this;
+    }
+
+    @Step
+    public String getTitleText(){
+        return getText(titleText);
+    }
+}
