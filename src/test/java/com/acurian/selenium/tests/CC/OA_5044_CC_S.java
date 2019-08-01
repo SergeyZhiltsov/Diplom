259ed25a8696ea4094014d1490f20adcb98f5d0b
Anna Pack <anna.pack@acurian.com>
2019-08-01 20:11:15 +0300

    HFL_4722_OLS fixed OA_5044_CC_S fixed DIA_4241_CC_ver3 - activation of EFC15337 protocol GERD_4301CC fixed

--- src/test/java/com/acurian/selenium/tests/CC/OA_5044_CC_S.java
+++ src/test/java/com/acurian/selenium/tests/CC/OA_5044_CC_S.java
@@ -544,7 +544,8 @@ public class OA_5044_CC_S extends BaseTest {
                 .waitForPageLoadSyn()
                 .assertVariables("Acurian", "Trial", "09/09/1980", "US",
                         "Dover, DE", site.zipCode, "qa.acurian@gmail.com", "999 -999-9999",
-                        "aut5044test", "AUT_OA_5044_Syn", "REGXXXOSA758")
+                                   env.equals("STG") ? "aut5044test" : "AUT_OA_5044_Syn", "AUT_OA_5044_Syn",
+                        "REGXXXOSA758")
                 .clickOnAnswer("[Successful direct schedule in clinical conductor]")
                 .clickNextButton(selectActionPageCC)
                 .waitForPageLoad()
