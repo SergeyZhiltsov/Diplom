c2e1ac431b40d6b233d242acec1e77204bac1a2e
Roman Gorbenko <rgorbenko@acurian.com>
2019-08-20 14:41:30 +0300

    Fixed after test run.

--- src/test/java/com/acurian/selenium/tests/OLS/ABRandomnessTest.java
+++ src/test/java/com/acurian/selenium/tests/OLS/ABRandomnessTest.java
@@ -7,10 +7,14 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.Test;
+import ru.yandex.qatools.allure.annotations.Step;
 import ru.yandex.qatools.ashot.AShot;
 import ru.yandex.qatools.ashot.coordinates.WebDriverCoordsProvider;
+
+import java.net.URISyntaxException;
 
 public class ABRandomnessTest extends BaseTest {
     private int countA = 0;
@@ -28,6 +32,7 @@ public class ABRandomnessTest extends BaseTest {
     }
 
     @Test(invocationCount = 100, skipFailedInvocations = true)
+    @Step("{0}")
     public void abRandomnes() {
         String testURL = "https://test-screener.acurian.com/questionnaire_test_staging_redirector/welcome?pn=800AMS1TST";
 
@@ -35,17 +40,23 @@ public class ABRandomnessTest extends BaseTest {
         options.addArguments("--incognito");
         DesiredCapabilities capabilities = DesiredCapabilities.chrome();
         capabilities.setCapability(ChromeOptions.CAPABILITY, options);
-        System.setProperty("webdriver.chrome.driver", ABRandomnessTest.class.getResource("/chromedriver.exe").getPath());
+        try {
+            System.setProperty("webdriver.chrome.driver", ABRandomnessTest.class.getResource("/chromedriver.exe").toURI().getPath());
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+        }
         WebDriver webDriver = new ChromeDriver(options);
         webDriver.manage().window().setSize(new Dimension(1400, 1050));
 
-        webDriver.navigate().to(testURL);
+                webDriver.navigate().to(testURL);
         String title = webDriver.getTitle();
         System.out.println(title);
         if (title.equals("Acurian Clinical Screener")) {
             countA++;
+            Assert.assertEquals(title, "Acurian Clinical Screener");
         } else {
             countB++;
+            Assert.assertEquals(title, "Screener");
         }
         totalCount++;
         TestListener.attachScreenshot(new AShot().coordsProvider(new WebDriverCoordsProvider()).takeScreenshot(webDriver));
@@ -53,6 +64,7 @@ public class ABRandomnessTest extends BaseTest {
     }
 
     @AfterClass
+    @Step("{0}")
     public void showResults() {
         System.out.println("Total count of test run: " + totalCount);
         System.out.println("Count for Acurian: " + countA);
