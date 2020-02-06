68e8e7f0c9660285e7a1f31d315e6be6ed720f6c
IvanLipskyi <lips.ivan5@gmail.com>
2020-02-06 11:31:49 +0200

    Release 89.1 added pages and fixed classes

---
+++ src/test/java/com/acurian/selenium/pages/CC/Diabetes_4356A/LiverBiopsyConfirmsNASHDiagnosisCC.java
@@ -0,0 +1,43 @@
+package com.acurian.selenium.pages.CC.Diabetes_4356A;
+
+import com.acurian.selenium.constants.Locators;
+import com.acurian.selenium.pages.CC.MainPageCC;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import ru.yandex.qatools.allure.annotations.Step;
+
+import java.util.List;
+
+public class LiverBiopsyConfirmsNASHDiagnosisCC extends MainPageCC {
+
+    public final String titleExpected = "Have you had a liver biopsy that confirms your NASH diagnosis?";
+
+    @FindBy(xpath = Locators.BASIC_TITLE_WITH_RADIO_BUTTON_OLS)
+    WebElement titleText;
+
+    @FindBy(xpath = Locators.RADIO_BUTTON_LIST_OLS)
+    List<WebElement> radioButtonsList;
+
+    public LiverBiopsyConfirmsNASHDiagnosisCC() {
+        PageFactory.initElements(getDriver(), this);
+    }
+
+    @Step
+    public LiverBiopsyConfirmsNASHDiagnosisCC waitForPageLoad() {
+        waitForPageLoadMain(titleText, titleExpected);
+        return this;
+    }
+
+    @Step
+    public LiverBiopsyConfirmsNASHDiagnosisCC clickOnAnswer(String answerText) {
+        clickOnRadioButton(radioButtonsList, answerText);
+        return this;
+    }
+
+    @Step
+    public String getTitleText(){
+        return getText(titleText);
+    }
+
+}
--- src/test/java/com/acurian/selenium/pages/OLS/generalHealth/WhichOfFollowingHaveYouDiagnosedWith_LiverDiseaseOLS.java
+++ src/test/java/com/acurian/selenium/pages/OLS/generalHealth/WhichOfFollowingHaveYouDiagnosedWith_LiverDiseaseOLS.java
@@ -10,14 +10,22 @@ import ru.yandex.qatools.allure.annotations.Step;
 
 public class WhichOfFollowingHaveYouDiagnosedWith_LiverDiseaseOLS extends MainPageOLS {
 
-    public final String titleExpected = "You reported that you have liver problems.\n" +
-            "Which of the following have you been diagnosed with?\n" +
+//    public final String titleExpected = "You reported that you have liver problems.\n" +
+//            "Which of the following have you been diagnosed with?\n" +
+//            "Please select all that apply.";
+    public final String titleExpected = "Do you have any of the following liver conditions?\n" +
             "Please select all that apply.";
 
     @FindBy(xpath = Locators.BASIC_TITLE_WITH_CHECKBOXES_BUTTON_OLS)
     WebElement titleText;
     @FindBy(xpath = Locators.CHEKBOX_LIST_OLS)
     List<WebElement> checkBoxList;
+
+//    @Step
+//    public WhichOfFollowingHaveYouDiagnosedWith_LiverDiseaseOLS waitForPageLoad() {
+//        waitForPageLoadMain(titleText, titleExpected);
+//        return this;
+//    }
 
     @Step
     public WhichOfFollowingHaveYouDiagnosedWith_LiverDiseaseOLS waitForPageLoad() {
