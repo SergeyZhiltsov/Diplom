013ac7f393b61b5e96751ec1ee08b6fff2321160
szhyltsov <zhyltsovs@gmail.com>
2020-03-05 13:44:31 +0200

    fixes+r93

--- src/test/java/com/acurian/selenium/utils/db/CheckTestSiteFlag.java
+++ src/test/java/com/acurian/selenium/utils/db/CheckTestSiteFlag.java
@@ -1,13 +1,11 @@
 package com.acurian.selenium.utils.db;
 
-import com.acurian.selenium.pages.BaseTest;
 import com.acurian.selenium.utils.DBConnection;
-import org.testng.annotations.AfterMethod;
 import org.testng.annotations.Test;
 
 
 
-public class CheckTestSiteFlag extends BaseTest {
+public class CheckTestSiteFlag {
     @Test()
     public void checkFlag() {
         final String env = System.getProperty("acurian.env", "STG");
