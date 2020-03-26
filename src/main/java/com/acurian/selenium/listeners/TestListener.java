5db076be3ec1885d684fe31133cea6a86f0a336b
User <gemantus2@ukr.net>
2020-03-26 14:54:49 +0200

    rerun for tests add

--- src/main/java/com/acurian/selenium/listeners/TestListener.java
+++ src/main/java/com/acurian/selenium/listeners/TestListener.java
@@ -1,12 +1,12 @@
 package com.acurian.selenium.listeners;
 
 import com.acurian.selenium.pages.BaseTest;
+import com.acurian.utils.analyzer.RetryAnalyzer;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.testng.ITestResult;
-import org.testng.TestListenerAdapter;
+import org.testng.*;
 import ru.yandex.qatools.allure.annotations.Attachment;
 import ru.yandex.qatools.ashot.Screenshot;
 import ru.yandex.qatools.ashot.comparison.ImageDiff;
@@ -14,15 +14,46 @@ import ru.yandex.qatools.ashot.comparison.ImageDiff;
 import javax.imageio.ImageIO;
 import java.io.ByteArrayOutputStream;
 import java.util.Arrays;
+import java.util.Iterator;
 
-public class TestListener extends TestListenerAdapter {
+public class TestListener extends TestListenerAdapter implements ITestListener {
 
     private static Logger Log = LogManager.getLogger(TestListener.class.getName());
 
     @Override
     public void onTestFailure(ITestResult result) {
-        Log.error("Test is unsuccessful. Test case - " + result.getMethod().getMethodName() + " has failed");
-        attachScreenshot();
+        //add allure attach
+        if (result.getMethod().getRetryAnalyzer() != null) {
+            RetryAnalyzer retryAnalyzer = (RetryAnalyzer) result.getMethod().getRetryAnalyzer();
+
+            if (retryAnalyzer.isRetryAvailable()) {
+                // do nothing
+            } else {
+
+                Log.error("Test has failed. Test case - " + result.getMethod().getMethodName() +
+                        " has failed due to reason:  " + result.getThrowable());
+                attachScreenshot();
+                //add allure attach
+            }
+        }
+        result.setStatus(ITestResult.FAILURE);
+    }
+
+    @Override
+    public void onFinish(ITestContext context) {
+        Iterator<ITestResult> failedTestCases = context.getFailedTests().getAllResults().iterator();
+        while (failedTestCases.hasNext()) {
+            ITestResult failedTestCase = failedTestCases.next();
+            ITestNGMethod method = failedTestCase.getMethod();
+            if (context.getFailedTests().getResults(method).size() > 1) {
+                failedTestCases.remove();
+            } else {
+
+                if (context.getPassedTests().getResults(method).size() > 0) {
+                    failedTestCases.remove();
+                }
+            }
+        }
     }
 
     @Override
