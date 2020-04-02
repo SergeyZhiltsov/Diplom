2ce8d6bbb0631ffd6691921dd8cded0566274eee
SergeyZhiltsov <zhyltsovs@gmail.com>
2020-04-02 17:29:52 +0300

    fast fix

--- src/test/java/com/acurian/selenium/dispoBlinx/Dispo43C.java
+++ src/test/java/com/acurian/selenium/dispoBlinx/Dispo43C.java
@@ -84,8 +84,8 @@ public class Dispo43C extends BaseTest {
                 .clickNextButton(identificationPageOLS)
                 .waitForPageLoadGMEGA()
                 .clickNextButton(new SiteSelectionPageOLS())
-                .waitForPageLoad(env.equals("QA") ? "Arthritis,a low back pain study,a rheumatoid arthritis (RA)" :
-                        "Arthritis, a low back pain study, a rheumatoid arthritis (RA)")
+                .waitForPageLoad(env.equals("QA") ? "Arthritis,a low back pain study,a rheumatoid arthritis (RA) study" :
+                        "Arthritis, a low back pain study, a rheumatoid arthritis (RA) study")
                 .getPID()
                 .clickOnFacilityName(site.name)
                 .clickNextButton(new QualifiedClose2PageOLS())
